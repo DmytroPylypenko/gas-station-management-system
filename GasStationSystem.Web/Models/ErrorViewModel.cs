1e09f504c76ba679c5cfa94cce39bf6cf92ca8a7
DmytroPylypenko <pilya.dima@gmail.com>
2025-11-18 11:01:48 +0200

    Initial commit: Project setup

---
+++ GasStationSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GasStationSystem.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
